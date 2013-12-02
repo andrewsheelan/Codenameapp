d1526de925c9f7855f5b6729bda1f016f1b17724
Andrew & Ken <Andrewken@livegamer.com>
2013-12-02 16:40:04 -0500

    Adding files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-  after_action :allow_iframe
+  after_action :allow_iframe, :index
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def index
+    
+  end
+
   def allow_iframe
     puts "Entered allow frame"
     response.headers['X-Frame-Options'] = 'ALLOW-FROM http://codenameapp.herokuapp.com/'
