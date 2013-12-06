e56bfe75004869685f45fcef376b11d0d2da728f
Andrew Sheelan Paliyan <andrewsheelan@yahoo.com>
2013-12-05 21:36:42 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Codenameapp::Application.routes.draw do
   post 'user/login' => 'application#login'
+  get 'user/login' => 'application#login'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
